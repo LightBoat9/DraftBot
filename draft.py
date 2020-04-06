144742e6cc3f4e93525cfc2b891510770c528992
Deniz <denizkerim@yahoo.com>
2020-04-05 23:40:51 -0500

    start and join session id

--- draft.py
+++ draft.py
@@ -2,8 +2,13 @@ from discord import *
 import asyncio
 import json
 import random
+from session import DraftSession
 
 client: Client = Client()
+
+# Global Variables
+COMMAND_PREFIX = "!"
+SESSIONS = {}
 
 async def main() -> None:
 	# Open secrets file and start with bot token
@@ -20,17 +25,33 @@ async def on_ready() -> None:
 
 @client.event
 async def on_message(message: Message) -> None:
-	print(message.author, message.content)
+	# print(message.author, message.content)
+	split_message = message.content.split(' ')
 
-	if message.content == "Start Draft":
+	if message.content == COMMAND_PREFIX + "draft":
+		channel = await set_channel(message)
 
-		# set channel, check if a dm exists or create dm
-		channel = message.author.dm_channel
-		if not channel:
-			await message.author.create_dm()
-			channel = message.author.dm_channel
+		# creating session and adding it to active sessions
+		session = DraftSession()
+		SESSIONS[str(session.session_id)] = session
+		await channel.send(
+			"- SETTING UP DRAFT -\nShare Session ID with Opposing Captian: `" + str(session.session_id) + "`"
+		)
 
-		await channel.send("Starting Draft")
+	if split_message[0] == COMMAND_PREFIX + "join":
+		channel = await set_channel(message)
+
+		# check if valid session
+		if split_message[1] in SESSIONS.keys():
+			await channel.send("nice")
+		else:
+			await channel.send("not a valid session id")
+
+# set channel, check if a dm exists or create dm
+async def set_channel(M):
+	if not M.author.dm_channel:
+		await M.author.create_dm()
+	return M.author.dm_channel
 
 if __name__ == "__main__":
 	loop = asyncio.get_event_loop()
--- session.py
+++ session.py
@@ -4,4 +4,3 @@ import enum
 class DraftSession(object):
     def __init__(self) -> None:
         self.session_id = uuid.uuid4()
-    
